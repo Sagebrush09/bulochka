100f2c024c57f63579baa7f08bf21e06e790f18f
polina <polina@SAGEBRUSH>
2024-04-03 17:44:04 +0300

    Добавьте файлы проекта.

---
+++ bulochka/Pages/EditInfoWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace bulochka.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для EditInfoWindow.xaml
+    /// </summary>
+    public partial class EditInfoWindow : Window
+    {
+        private Student _currentStudent = new Student();
+        L4Entities bd = new L4Entities();
+
+        public EditInfoWindow(Student selectStudent)
+        {
+            InitializeComponent();
+            if(selectStudent != null)
+            {
+                _currentStudent = selectStudent;
+                DataContext = _currentStudent;
+                ComboBoxEditSpecialization.ItemsSource = L4Entities.GetContext().Specialization.ToList();
+                ComboBoxEditFaculty.ItemsSource = L4Entities.GetContext().Faculty.ToList();
+            }
+        }
+   
+        private void ButtonEdit_Click(object sender, RoutedEventArgs e)
+        {
+            bool flag = true;
+            foreach (Control c in GridEdit.Children)
+            {
+                if (c.GetType() == typeof(TextBox))
+                {
+                    if (((TextBox)c).Text == String.Empty)
+                    {
+                        flag = false;
+                    }
+                }
+
+                if (c.GetType() == typeof(ComboBox))
+                {
+                    if (((ComboBox)c).Text == String.Empty)
+                    {
+                        flag = false;
+                    }
+                }
+            }
+            if (flag)
+            {
+                L4Entities.GetContext().SaveChanges();
+                MessageBox.Show("Ура! Изменилось!");
+                MainWindow _mainWindow = new MainWindow();
+                _mainWindow.Show();
+                this.Close();
+            }
+            else MessageBox.Show("Пусто и грустно :( ");
+
+        }
+
+        private void ButtonEditBack_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow _mainWindow = new MainWindow();
+            _mainWindow.Show();
+            this.Close();
+        }
+
+        private void TextBoxEditCourse_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBoxEditName_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            foreach (char c in e.Text)
+            {
+                if (!char.IsLetter(c))
+                {
+                    e.Handled = true;
+                    break;
+                }
+            }
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                L4Entities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+            }
+        }
+    }
+}
---
+++ bulochka/Pages/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bulochka
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Student _currentStudent = new Student();
+        L4Entities bd = new L4Entities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = _currentStudent;
+            DataGridStudent.ItemsSource = L4Entities.GetContext().Student.ToList();
+            DataGridStudent.ItemsSource = L4Entities.GetContext().Student.OrderBy(x => x.IDStudent).ToList();
+        }
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Pages.AddInfoWindow _addInfoWindow = new Pages.AddInfoWindow();
+            _addInfoWindow.Show();
+            this.Close();
+        }
+
+        private void ButtonDeleteGrid_Click(object sender, RoutedEventArgs e)
+        {
+            var StudentRemoving = DataGridStudent.SelectedItems.Cast<Student>().ToList();
+            if (MessageBox.Show($"Подтвердить удаление?", "!",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                L4Entities.GetContext().Student.RemoveRange(StudentRemoving);
+                L4Entities.GetContext().SaveChanges();
+                MessageBox.Show("Ура! Удалилось!");
+                DataGridStudent.ItemsSource = L4Entities.GetContext().Student.OrderBy(x => x.IDStudent).ToList();
+            }
+        }
+
+        private void ButtonEditGrid_Click(object sender, RoutedEventArgs e)
+        {
+            Student item = (Student)DataGridStudent.SelectedItem;
+            Pages.EditInfoWindow _editInfoWindow = new Pages.EditInfoWindow(item);
+            _editInfoWindow.Show();
+            this.Close();
+        }
+    }
+}
